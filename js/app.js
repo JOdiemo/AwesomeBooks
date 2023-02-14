c2ace07b04401857c0e796e187e72d5bb5d5076d
evillaz <evillaz123@gmail.com>
2023-02-14 08:34:22 -0500

    update class and methods

--- js/app.js
+++ js/app.js
@@ -4,52 +4,39 @@
 /* eslint-disable-next-line quotes */
 const addBtn = document.getElementsByClassName('addBtn')[0];
 const bookLibrary = document.getElementsByClassName('library')[0];
-let books = [];
 
-addBtn.addEventListener('click', (e) => {
+class Book {
+  constructor(author, title){
+  this.tile = title;
+  this.author = author;
+  }
+}
+
+class BookArray  {
+  constructor() {
+    this.books = JSON.parse(localStorage.getItem('library')) || [];
+  }
+
+  addBooks (author, title) {
+    if (author !== '' && title !== ''){
+      const book = new Book(author, title);
+      this.books.push(book);
+      localStorage.setItem('library', JSON.stringify(this.books));
+    }
+  }
+
+  remove (field)  {
+    const BookIndex = this.books.findIndex((i) => i === field);
+    this.books.splice(BookIndex, 1);
+    localStorage.setItem('library', JSON.stringify(this.books));
+  }
+}
+
+const library = new BookArray();
+
+addBtn.addEventListener('click', () => {
   const author = document.getElementById('author').value.trim();
   const title = document.getElementById('title').value.trim();
-  addBooks(author, title);
-  display();
-})
-
-const addBooks = (Author, Title) => {
-  if (Author !== '' && Title !== ''){
-    const obj = {
-      title: Title,
-      author: Author,
-    };
-    books.push(obj);
-    localStorage.setItem('Book1', JSON.stringify(books));
-  }
-};
-
-function display() {
-  if (localStorage.getItem('Book1') == null) {
-    books = [];
-  } else {
-    books = JSON.parse(localStorage.getItem('Book1'));
-  }
-  let bookDisplay = ``;
-  books.forEach((element, i) => {
-    bookDisplay += `
-    <div id= "book${i}">
-    <p>${element.title}</p>
-    <p>${element.author}</p>
-    <button onclick = 'remove(${i})' >Remove</button>
-    </div>
-    `
-   });
-  bookLibrary.innerHTML = bookDisplay;
-}
-
-const remove = (field) => {
-  const BookIndex = books.findIndex((item, i) => i === field);
-  books.splice(BookIndex, 1);
-  localStorage.setItem('Book1', JSON.stringify(books));
-  display();
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-  display();
+  library.addBooks(author, title);
+  console.log(library);
 });
